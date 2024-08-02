475e7628737e68d84a660dead447303679e35203
Jacob Candelaria <jacobc.52@hotmail.com>
2024-08-02 02:02:38 -0600

    Completed Lab implementing like/dislike buttons w/ counter

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,50 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+
+      handleLike:() => { // Like Button Event Handler
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+
+      handleDislikes:() => { // Dislike Button Event Handler
+        this.setState((prevState) =>({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+    };
   }
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <div className='content-rating'>
+        <p>
+          Add Text Here
+        </p>
+
+        <div className='rating-buttons'>
+            <button className='like-button' onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+
+            <button className='dislike-button' onClick={this.state.handleDislikes}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+
+          <div>
+            <p>
+              Total Ratings: {this.state.totalRatings}
+            </p>
+          </div>
+      </div>
     );
   }
 }
